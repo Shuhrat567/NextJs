b2c0a93981882e699ddf6eede1139c2eca3c0cbb
shuhrat567 <isoilovshuhrat567@gmail.com>
2024-02-07 21:43:28 +0300

    commits

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" p-24">
+      <h1>Next.js Auth Tutorial</h1>
+    </main>
+  );
+}
---
+++ my-app/src/constant/index.ts
@@ -0,0 +1,26 @@
+export const MenuLinks = [
+    { href: '/', key: 'Home', text: 'Home' },
+    { href: '/', key: 'Our Services', text: 'Our Services' },
+    { href: '/', key: 'Projects', text: 'Projects' },
+    { href: '/', key: 'Blog', text: 'Blog' },
+    { href: '/', key: 'About', text: 'About' }
+  ];
+
+export const ProvideLinks = [
+    { title: 'App Design', img: '/app.png', bg: '#26C6DA' },
+    { title: 'Web Design', img: '/web.png', bg: '#FF733B' },
+    { title: 'Web Development', img: '/web-d.png', bg: '#be63f9' },
+]
+
+export const Tick = [
+  {img: '/tick.png', alt: 'tick', info: "Blandit ut nunc vel bibendum semper elit"},
+  {img: '/tick.png', alt: 'tick', info: "Blandit ut nunc vel bibendum"},
+]
+
+export const TextLength = [
+  { title: 'Our Projects',},
+  { title: 'What We Provide',},
+]
+
+
+  
